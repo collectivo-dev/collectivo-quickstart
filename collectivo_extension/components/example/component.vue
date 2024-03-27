8347f62088befebba638eba5cf09397af407ee23
Joël <57440945+jofmi@users.noreply.github.com>
2024-03-27 15:44:02 +0100

    feat: add example content

---
+++ collectivo_extension/components/example/component.vue
@@ -0,0 +1,8 @@
+<template>
+  <CollectivoCard title="Example component">
+    <template #content>
+      This is a custom component. The source code can be found in
+      'components/example/component.vue'.
+    </template>
+  </CollectivoCard>
+</template>
---
+++ collectivo_extension/pages/example/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+// Requires authentication to view this page
+definePageMeta({
+  middleware: ["auth"],
+});
+
+// Set the title of the page
+setCollectivoTitle("Example Page");
+</script>
+
+<template>
+  <CollectivoContainer>
+    <p>
+      Hello World! This is a custom page. The source code can be found in
+      'pages/example/index.vue'. Use vue components and tailwind classes to
+      design your pages. You can replace 'example' with the name of your
+      extension. Namespacing will avoid conflicts with other extensions.
+    </p>
+  </CollectivoContainer>
+
+  <ExampleComponent></ExampleComponent>
+</template>
